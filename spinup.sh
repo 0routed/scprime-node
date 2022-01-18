d42ac7511e0c59b609423183f01a9b88d717eddf
0routed <97963496+0routed@users.noreply.github.com>
2022-01-18 17:03:48 +0100

    init

---
+++ spinup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#adjust as needed
+NODENAME=SCP0X # nodename for the container
+HA=8282 #will be passed to spd in the container --host-addr :$HA
+SA=8283 #will be passed to spd in the container --siamux-addr :$SA
+HAA=8285 #will be passed to spd in the container --host-api-addr :$HAA
+SCVERSION=1.6.0 #choose version of spd - only tested with 1.6.0
+SCTIMEZONE=Europe/Berlin # set your timezone at buildtime
+
+docker build --pull --build-arg SCVERSION=$SCVERSION --build-arg SETTZ=$SCTIMEZONE  --rm -f "Dockerfile" -t local/scprime-node:$SCVERSION "." # comment out this line if you just need an additional container
+
+docker run -dt \
+ -v /path/on/host/to/metadata:/home/appuser/.scprime \
+ -v /path/on/host/to/storage:/mnt/storage \
+ -p $HA:$HA \
+ -p $SA:$SA \
+ -p $HAA:$HAA \
+ -e HA=$HA \
+ -e SA=$SA \
+ -e HAA=$HAA \
+ --name $NODENAME \
+ --cpus=3 \
+ --restart unless-stopped\
+ local/scprime-node:$SCVERSION
