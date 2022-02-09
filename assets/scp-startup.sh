93a31938b264fde49764f5b3a18cd40cedd9a1c5
0routed <97963496+0routed@users.noreply.github.com>
2022-02-09 21:23:12 +0100

    v1.0-1.6.0

--- assets/scp-startup.sh
+++ assets/scp-startup.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-#johndoe220117
+#johndoe220209
 
 touch .scprime/startuplog
 echo '--#-#-#--' | tee -a .scprime/startuplog
 echo $(date "+%Y-%m-%d_%H:%M:%S.%N") ': startupscript...' | tee -a .scprime/startuplog
+
+#to speedup the initial setup, if there is no consensus.db the latest version is downloaded from https://consensus.scpri.me/releases/consensus-latest.zip
+if [ ! -e ~/.scprime/consensus/consensus.db ];
+then
+   echo $(date "+%Y-%m-%d_%H:%M:%S.%N") ': no consensus.db found... bootstrapping. download can take a few minutes.' | tee -a .scprime/startuplog
+   mkdir ~/.scprime/consensus
+   curl https://consensus.scpri.me/releases/consensus-latest.zip --output ~/.scprime/consensus/latest.zip
+   echo $(date "+%Y-%m-%d_%H:%M:%S.%N") ': download done. unpacking.' | tee -a .scprime/startuplog
+   unzip ~/.scprime/consensus/latest.zip -d ~/.scprime/consensus/ | tee -a .scprime/startuplog
+   rm ~/.scprime/consensus/latest.zip
+fi
 
 #if there is a walletpassword it is loaded as environmentvariable to auto-unlock the wallet (spd only attemps auto-unlock when consensus is fully synced, takes a long time on first setup!!!)
 if [ -e .scprime/walletpassword ];
